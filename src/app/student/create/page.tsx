6952c8f91e4c03c9b23505d712d5ef32cfcb5536
Abhijit Saha <work.abhijitsaha@gmail.com>
2025-12-05 13:13:00 +0530

    teacher and student create page card and metadata

--- src/app/student/create/page.tsx
+++ src/app/student/create/page.tsx
@@ -1,5 +1,30 @@
+import {
+	Card,
+	CardContent,
+	CardHeader,
+	CardTitle,
+} from "@/components/shadcnui/card";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Student Create | User CRUD App",
+	description: "Student creation page of User CRUD Application",
+};
+
 const page = () => {
-	return <div>Enter</div>;
+	return (
+		<section className="grid h-[90dvh] place-items-center">
+			<Card className="w-xs">
+				<CardHeader>
+					<CardTitle className="text-center text-2xl font-semibold">
+						Create Student
+					</CardTitle>
+				</CardHeader>
+
+				<CardContent></CardContent>
+			</Card>
+		</section>
+	);
 };
 
 export default page;
