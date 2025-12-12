ee512e4c4c37e3980ff966ce504724036b9b06cc
Abhijit Saha <work.abhijitsaha@gmail.com>
2025-12-12 23:31:29 +0530

    student form schema and type created

--- src/lib/zodSchema.ts
+++ src/lib/zodSchema.ts
@@ -5,4 +5,14 @@ export const teacherFormSchema = z.object({
 	lastName: z.string().min(3, "Last Name must be more that 3 characters!"),
 });
 
+export const studentFormSchema = z.object({
+	firstName: z.string().min(2, "First Name must be more that 2 characters!"),
+	lastName: z.string().min(3, "Last Name must be more that 3 characters!"),
+	gender: z.string({ error: "Select Your Gender" }),
+	email: z.email({ error: "Invalid Email Address" }),
+	teacherId: z.string(),
+});
+
 export type TeacherFormType = z.infer<typeof teacherFormSchema>;
+
+export type StudentFormType = z.infer<typeof studentFormSchema>;
